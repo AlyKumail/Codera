3f7828c43e254d6a81e14fe1f268adabc336f3cf
kumail <alikumail1100@gmail.com>
2022-11-17 18:31:51 +0500

    dashboard update & mob nav

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const crossbarLogin = document.getElementById("crossbarLogin");
 const joinNow = document.getElementById("joinNow");
 const loginBtn = document.getElementById("loginBtn");
 
+const joinNowM = document.getElementById("joinNowM");
+const loginBtnM = document.getElementById("loginBtnM");
+
 const loginModal = document.getElementById("loginModal");
 const joinModal = document.getElementById("joinModal");
 
@@ -42,12 +45,28 @@ loginBtn.addEventListener("click", () => {
     console.log("joinnow");
 });
 
+joinNowM.addEventListener("click", () => {
+    // if (loginModal.classList.contains("displayNav")) {
+    joinModal.classList.add("displayNav");
+    document.body.style.overflow = "hidden";
+    // }
+    console.log("joinnow");
+});
+loginBtnM.addEventListener("click", () => {
+    // if (loginModal.classList.contains("displayNav")) {
+    loginModal.classList.add("displayNav");
+    document.body.style.overflow = "hidden";
+    // }
+    console.log("joinnow");
+});
+
 const hamburger = document.getElementById("hamburger");
 const mobileNav = document.getElementById("mobileNav");
 const navCross = document.getElementById("navCross");
 
 hamburger.addEventListener("click", () => {
     mobileNav.style.display = "flex";
+    console.log("ham");
 });
 navCross.addEventListener("click", () => {
     mobileNav.style.display = "none";
